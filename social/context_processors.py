15b5be5eb23260fd64be3d439dbd220ce0ae787f
sdb <stefan@ellefant.be>
2010-08-29 22:29:57 +0200

    small bugfixes

--- social/context_processors.py
+++ social/context_processors.py
@@ -8,7 +8,7 @@ from django.conf import settings
 def social(request):
   # TODO handle case: no services found in database
   last_update = Service.objects.aggregate(Max('updated'))['updated__max']
-  elapsed = (datetime.utcnow() - last_update).seconds / 60
+  elapsed = (datetime.utcnow() - last_update).seconds / 60 if last_update != None else 1000
   return {"title":settings.APP_TITLE,
           "last_update":last_update,
           "elapsed":elapsed,
