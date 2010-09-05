ace223d3cf880539169c44c9f0029345a37aa943
sdb <stefan@ellefant.be>
2010-09-05 18:38:57 +0200

    added basic search functionality

--- social/urls.py
+++ social/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import *
 
-from sdb.social.views import index, update
+from sdb.social.views import index, update, search_with_param, search
 
 urlpatterns = patterns('',
   url(r'^update$', update, name='update'),
+  url(r'^search$', search_with_param, name='search_with_param'),
+  url(r'^search/(?P<q>.+)$', search, name='search'),
   url(r'^$', index, name='social'),
   url(r'^(?P<typ>\w+)/$', index, name='social'),
 )
--- social/views.py
+++ social/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.conf import settings
+from django.utils.http import urlquote_plus
+from django.db.models import Q
 
 import simplejson as json
 
@@ -13,6 +15,34 @@ def index(request, typ=""):
   updates = []
   per_page = settings.ENTRIES_PER_PAGE
   entries = Entry.objects.order_by('-pub_date')[:per_page] if typ == '' else Entry.objects.filter(typ=typ).order_by('-pub_date')[:per_page]
+  updates = prepare_entries(entries)
+  return render_to_response('social/index.html', {'updates':updates}, context_instance=RequestContext(request))
+
+
+def update(request):
+  import updater
+  updater.update()
+  return HttpResponseRedirect(request.GET.get('redirect') if 'redirect' in request.GET else reverse('social'))
+
+
+def search_with_param(request):
+  return HttpResponseRedirect(reverse('search', args=[urlquote_plus(request.GET.get('search'))]))
+
+
+def search(request, q):
+  per_page = settings.ENTRIES_PER_PAGE
+  terms = q.split("+")
+  queries = [Q(data__contains=term) for term in terms]
+  query = queries.pop()
+  for item in queries:
+    query &= item
+  entries = Entry.objects.filter(query).order_by('-pub_date')[:per_page]
+  updates = prepare_entries(entries)
+  return render_to_response('social/index.html', {'updates':updates, 'query':q.replace('+', ' ')}, context_instance=RequestContext(request))
+
+
+def prepare_entries(entries):
+  updates = []
   for entry in entries:
     update = {'pub_date':entry.pub_date}
     update['entry'] = entry
@@ -21,10 +51,7 @@ def index(request, typ=""):
     else:
       update['data'] = json.loads(entry.data)
     updates.append(update)
-  return render_to_response('social/index.html', {'updates':updates}, context_instance=RequestContext(request))
+  return updates
+  
 
 
-def update(request):
-  import updater
-  updater.update()
-  return HttpResponseRedirect(request.GET.get('redirect') if 'redirect' in request.GET else reverse('social'))
