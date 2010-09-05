cadaebb9b1130ff3e7afca85dbe175a7aee8d95d
sdb <stefan@ellefant.be>
2010-09-05 14:27:20 +0200

    added reset action to admin interface

--- social/admin.py
+++ social/admin.py
@@ -1,8 +1,16 @@
 from sdb.social.models import Service, Entry, Link
 from django.contrib import admin
+from datetime import datetime
+
+def reset_service(modeladmin, request, queryset):
+  queryset.update(updated=datetime.min)
+  for s in queryset:
+    Entry.objects.filter(service=s).delete()
+reset_service.short_description = 'Reset selected services'
 
 class ServiceAdmin(admin.ModelAdmin):
   list_display = ['title', 'name', 'desc', 'period', 'updated', 'args', 'props']
+  actions = [reset_service]
 
 class EntryAdmin(admin.ModelAdmin):
   list_display = ['desc', 'typ', 'service', 'pub_date', 'data']
