234a5d929d20aa1dab9293c256610b8724ef7156
sdb <stefan@ellefant.be>
2010-09-05 21:29:39 +0200

    moved social links

--- context_processors.py
+++ context_processors.py
@@ -1,8 +1,6 @@
-from sdb.social.models import Link
 from django.conf import settings
 
 def main(request):
   return {'title':settings.APP_TITLE,
-          'social':{'links':Link.objects.order_by('title')},
           'favicon':settings.FAVICON,
           'blog':settings.BLOG_URL if hasattr(settings, 'BLOG_URL') else None,}
--- social/context_processors.py
+++ social/context_processors.py
@@ -1,4 +1,4 @@
-from sdb.social.models import Service
+from sdb.social.models import Service, Link
 from sdb.social import updater
 
 from datetime import datetime
@@ -10,4 +10,5 @@ def social(request):
   last_update = Service.objects.aggregate(Max('updated'))['updated__max']
   elapsed = ((datetime.utcnow() - last_update).seconds / 60 if not updater.running_update else -1) if last_update != None else 1000
   return {'last_update':last_update,
+          'social':{'links':Link.objects.order_by('title')},
           'elapsed':elapsed,}
