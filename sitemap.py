3cda7c3765d15c3f66e8cd19a574372e6a40e45f
sdb <stefan@ellefant.be>
2010-09-22 02:26:46 +0200

    basic sitemap

---
+++ sitemap.py
@@ -0,0 +1,13 @@
+from django.contrib.sitemaps import Sitemap
+from django.db.models import Max
+from sdb.social.models import Entry
+from django.core.urlresolvers import reverse
+from datetime import datetime
+
+class MainSitemap(Sitemap):
+
+  def items(self):
+    return ['home', 'contact', 'social']
+
+  def location(self, obj):
+    return reverse(obj)
--- urls.py
+++ urls.py
@@ -1,15 +1,22 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.sitemaps import FlatPageSitemap, GenericSitemap
+from sdb.sitemap import MainSitemap
 
 admin.autodiscover()
+
+sitemaps = {
+    'flatpages': FlatPageSitemap,
+    'main' : MainSitemap,
+}
 
 urlpatterns = patterns('',
   (r'^home/', include('sdb.home.urls')),
   (r'^contact/', include('sdb.contact.urls')),
   (r'^stream/', include('sdb.social.urls')),
   (r'^admin/', include(admin.site.urls)),
-#  (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+  (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 )
 
 if settings.DEBUG:
