df6c0ec6c70391389d12100ef855ae431c6959eb
sdb <stefan@ellefant.be>
2010-09-29 20:33:45 +0200

    some thumbnails of flickr photos on the front page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,10 @@
-from django.views.generic.list_detail import object_list
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from social.models import Entry, Service
+from photos.models import Photo
 
 
 def index(request):
   entries = Entry.objects.all().order_by('-pub_date')[:5]
-  return object_list(request,
-    template_name='home/index.html',
-    queryset=entries
-  )
+  photos = Photo.objects.all().order_by('-date_taken')[:6]
+  return render_to_response('home/index.html', {'object_list':entries, "photos":photos}, context_instance=RequestContext(request))
