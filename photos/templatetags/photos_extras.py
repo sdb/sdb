6e4d4f1a31efdfd4f118e044f1637029acaa42f2
sdb <stefan@ellefant.be>
2010-10-01 00:15:17 +0200

    use lightbox to show photos from flickr

--- photos/templatetags/photos_extras.py
+++ photos/templatetags/photos_extras.py
@@ -8,3 +8,7 @@ register = template.Library()
 @register.filter(name='flickr_link')
 def flickr_link(photo):
   return 'http://www.flickr.com/photos/%s/%s' %(photo.data['owner'], photo.photo_id)
+
+@register.filter(name='flickr_photo')
+def flickr_photo(photo):
+  return 'http://farm%s.static.flickr.com/%s/%s_%s.jpg' %(photo.data['farm'], photo.data['server'], photo.photo_id, photo.data['secret'])
