1ce835bcce0051fe1bcb5575e6efef6698bac8c7
sdb <stefan@ellefant.be>
2010-09-30 23:34:32 +0200

    random pics on home page

--- home/views.py
+++ home/views.py
@@ -6,5 +6,5 @@ from photos.models import Photo
 
 def index(request):
   entries = Entry.objects.all().order_by('-pub_date')[:5]
-  photos = Photo.objects.all().order_by('-date_taken')[:6]
+  photos = Photo.objects.all().order_by('?')[:6]
   return render_to_response('home/index.html', {'object_list':entries, "photos":photos}, context_instance=RequestContext(request))
