6a4406a0e99618e11fbf00be4085e867821d303c
sdb <stefan@ellefant.be>
2010-12-11 00:53:02 +0100

    show list of latest posts on home page

--- home/views.py
+++ home/views.py
@@ -7,4 +7,5 @@ from photos.models import Photo
 def index(request):
   entries = Entry.objects.all().order_by('-pub_date')[:5]
   photos = Photo.objects.all().order_by('?')[:6]
-  return render_to_response('home/index.html', {'object_list':entries, "photos":photos}, context_instance=RequestContext(request))
+  posts = Entry.objects.filter(typ='post').order_by('-pub_date')[:5]
+  return render_to_response('home/index.html', {'object_list':entries, "photos":photos, 'posts':posts}, context_instance=RequestContext(request))
